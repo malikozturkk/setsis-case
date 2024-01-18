542940640525117e3028d26ff7445815894a1a54
Malik Öztürk <malik.ozturk@enuygun.com>
2024-01-18 13:43:59 +0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.infrastructureLogging = { debug: /PackFileCache/ }
-        return config;
-      }
+  webpack(config) {
+    config.infrastructureLogging = { debug: /PackFileCache/ }
+    return config;
+  }
 }
 
 module.exports = nextConfig
